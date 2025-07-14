f0a0c0f749afc0d389cc9420766c532a7cd93b6e
shahnoow <shahnawazjawed9@gmail.com>
2025-07-15 01:34:33 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,28 +9,26 @@ import { getAllFoodItems } from "./utils/supabaseFunction";
 import { actionType } from "./context/reducer";
 
 function App() {
-  const [{ food_items }, dispatch] = useStateValue();
+  const [, dispatch] = useStateValue(); // Remove 'food_items' since it's not used
 
-  const fetchData = async () => {
-    try {
-      const data = await getAllFoodItems();
-      dispatch({
-        type: actionType.SET_FOOD_ITEMS,
-        food_items: data,
-      });
-    } catch (error) {
-      console.error("Failed to fetch food items:", error);
-    }
-  };
+  // Define fetchData outside useEffect so we can add it to dependencies if needed
+  const fetchData = React.useCallback(async () => {
+    const data = await getAllFoodItems();
+    dispatch({
+      type: actionType.SET_FOOD_ITEMS,
+      food_items: data,
+    });
+  }, [dispatch]);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]); // Add fetchData here
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence exitBeforeEnter>
       <div className="flex flex-col w-screen h-auto bg-primary">
         <Header />
+
         <main className="w-full px-4 py-4 mt-14 md:mt-20 md:px-16">
           <Routes>
             <Route path="/*" element={<MainContainer />} />
